752e5aba9f4ff420cc3b622281f3be815f5c7e60
Jhonatan Carvalho <jhonatan.carvalho92@gmail.com>
2018-03-06 11:18:00 -0300

    Commit inicial

---
+++ src/main/java/br/com/jhonatan/apontadorhorasapi/ApontadorHorasApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.jhonatan.apontadorhorasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApontadorHorasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApontadorHorasApiApplication.class, args);
+	}
+}
